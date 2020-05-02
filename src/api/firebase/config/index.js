974ab4b47da66ee5bee8cafe069054a0b53b14af
Naod Yeheyes <naodya@gmail.com>
2020-05-02 19:32:51 -0400

    Added firebase hosting.

--- src/api/firebase/config/index.js
+++ src/api/firebase/config/index.js
@@ -5,8 +5,8 @@ const firebaseConfig = {
   projectId: 'ecrt-tracker',
   storageBucket: 'ecrt-tracker.appspot.com',
   messagingSenderId: '947035241094',
-  appId: '1:947035241094:web:f37b6b05b0707c77992f07',
-  measurementId: 'G-66Y8Y5HLCL',
+  appId: '1:947035241094:web:5d5daefb0d51e6a5992f07',
+  measurementId: 'G-QEW05YPM19',
 };
 
 export default firebaseConfig;
